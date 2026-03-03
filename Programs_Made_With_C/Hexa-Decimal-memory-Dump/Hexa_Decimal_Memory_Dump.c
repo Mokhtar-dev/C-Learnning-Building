5d84b9bbb4bdfc81ad7edc65c02125ce7bb05581
Mokhtar-dev <generalmk122@gmail.com>
2026-03-03 15:17:11 -0800

    Hexa Decimal Memory Dump that i dont know shit about

---
+++ Programs_Made_With_C/Hexa-Decimal-memory-Dump/Hexa_Decimal_Memory_Dump.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+int main()
+{
+    printf("\033[2j\033[?25l");
+    long addr = 0x7ffe0000;
+    while (1)
+    {
+        printf("\033[H\033[1;36m\n [ REAl-TIME MEMORY DUMP SCAN ]\n\n");
+        for (int i = 0; i < 12; i++)
+        {
+            printf("\033[1;33m%016lx: ", addr + i * 16);
+            for (int j = 0; j < 16; j++)
+            {
+                unsigned char byte = *((unsigned char *)(addr + i * 16 + j));
+                printf("%02x ", byte);
+            }
+            printf("\n");
+        }
+    }
+
+    return 0;
+}
