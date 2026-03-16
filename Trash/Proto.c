844aec294e9d9696e848f967309805afc1ca0ccc
Mokhtar-dev <generalmk122@gmail.com>
2026-03-15 17:06:21 -0700

    trash

---
+++ Trash/Proto.c
@@ -0,0 +1,34 @@
+#include <conio.h>
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    char x;
+    int num;
+    float j;
+
+    /*Accept the values for data types from user*/
+    printf("Enter Character:");
+    scanf("%c",&x);
+    printf("enter integer value:");
+    scanf("%i",&num);
+    printf("Enter a float value:");
+    scanf("%f",&j);
+    // constant slashes
+    /*
+    \a --> 
+    \n --> new line
+    \r --> carrige
+    \t --> 
+    \v -->
+    \' --> single quotes
+    \"" --> Doubel quotes
+    \? --> question mark
+    \\ --> basck slash
+    \0 --> null
+    */
+   
+
+    return 0;
+}
+
